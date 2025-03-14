b5c82e5bb872fbed7731d2a540ecf2198a5f05c9
Shaawwnn <shawnalberto10@gmail.com>
2025-03-15 01:25:01 +0800

    🚚 Move userSchema to the root schema dir

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,6 @@
+export * from './Booking';
+export * from './BookingStatus';
 export * from './Context';
+export * from './Service';
 export * from './User';
 export * from './UserRole';
---
+++ src/schema/UserSchema.ts
@@ -0,0 +1,47 @@
+import { IUser, UserRole } from '@models';
+import bcrypt from 'bcrypt';
+import mongoose, { Document, Schema, model } from 'mongoose';
+
+// Extend IUser with Document to work with Mongoose
+export interface IUserDocument extends Omit<IUser, '_id'>, Document {
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const UserSchema = new Schema<IUserDocument>({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  pronouns: { type: String, enum: ['he/him', 'she/her', 'they/them'] },
+  contactNumber: { type: String },
+  password: { type: String, required: false },
+  googleId: { type: String, required: false },
+  role: { type: String, enum: Object.values(UserRole), default: UserRole.Patron },
+  imageUrl: { type: String },
+  bio: { type: String },
+  rating: { type: Number },
+  createdAt: { type: Date, default: Date.now },
+  title: { type: String }
+});
+
+// ✅ Password hashing middleware
+UserSchema.pre<IUserDocument>('save', async function (next) {
+  if (!this.isModified('password')) return next(); // Only hash when password is modified
+
+  // User provides password only when they register/login using email and password
+  // Password is undefined for google auth users
+  if (!this.password) return next(); // Only hash when password is modified
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err as Error);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword: string) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export const User = model<IUserDocument>('User', UserSchema);

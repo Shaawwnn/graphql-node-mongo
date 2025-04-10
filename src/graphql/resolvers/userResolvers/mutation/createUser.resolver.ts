fb4446fb67bc4a95ddd6aae4a225483b5697ee8c
Shaawwnn <shawnalberto10@gmail.com>
2025-04-10 23:54:32 +0800

    ♻️ Send cookies for auto login

--- src/graphql/resolvers/userResolvers/mutation/createUser.resolver.ts
+++ src/graphql/resolvers/userResolvers/mutation/createUser.resolver.ts
@@ -1,4 +1,5 @@
-import { IUser, UserRole } from '@models';
+import { AuthUserContext, IUser, UserRole } from '@models';
+import jwt from 'jsonwebtoken';
 import { User } from 'schema/UserSchema';
 
 interface ICreateUserInput {
@@ -10,17 +11,33 @@ interface ICreateUserInput {
   contactNumber: string;
 }
 
-export const createUser = async (_: unknown, args: { userInput: ICreateUserInput }): Promise<IUser | undefined> => {
+export const createUser = async (
+  _: unknown,
+  args: { userInput: ICreateUserInput },
+  context: AuthUserContext
+): Promise<IUser | undefined> => {
+  const { res } = context;
+
   try {
     const { userInput } = args;
     const newUser = new User(userInput);
     const user = await newUser.save();
-    console.log(user);
+
     if (!user._id) {
       throw new Error('User document missing _id');
     }
     console.log(`🚀 ${user.firstName} successfully added!\n`);
+    const { JWT_SECRET } = process.env;
+    //
+    if (!JWT_SECRET) throw new Error('JWT_SECRET is undefined');
 
+    const userToken = jwt.sign({ uid: user._id, email: user.email, role: user.role }, JWT_SECRET);
+
+    res.cookie('userToken', userToken, {
+      httpOnly: true,
+      maxAge: 3600000,
+      secure: false
+    }); //1hr
     return user;
     //
   } catch (error) {
--- src/graphql/resolvers/userResolvers/query/me.resolver.ts
+++ src/graphql/resolvers/userResolvers/query/me.resolver.ts
@@ -2,7 +2,6 @@ import { AuthUserContext } from '@models';
 import { User } from 'schema/UserSchema';
 
 export const me = async (_: unknown, __: null, { authUser }: AuthUserContext) => {
-  console.log('called');
   if (!authUser) throw new Error('⚠ Unauthorized User');
   return await User.findById(authUser.uid);
 };
